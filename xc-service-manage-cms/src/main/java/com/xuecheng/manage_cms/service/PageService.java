aab470e2001b7a87a9ab92dcd317fc0718ee3fe4
zhouzhu85 <734837014@qq.com>
2019-07-30 17:18:58 +0800

    cms页面管理列表分页查询、条件筛选

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
@@ -6,10 +6,9 @@ import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.QueryResponseResult;
 import com.xuecheng.framework.model.response.QueryResult;
 import com.xuecheng.manage_cms.dao.CmsPageRepository;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 /**
@@ -30,6 +29,30 @@ public class PageService {
      * @return
      */
      public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest){
+         if (queryPageRequest==null){
+             queryPageRequest=new QueryPageRequest();
+         }
+         //自定义条件查询
+         //定义条件匹配器
+         ExampleMatcher exampleMatcher = ExampleMatcher.matching().withMatcher("pageAliase", ExampleMatcher.GenericPropertyMatchers.contains());
+         //条件对象
+         CmsPage cmsPage = new CmsPage();
+         /** 设置条件值 **/
+         //设置站点id
+         if (StringUtils.isNotEmpty(queryPageRequest.getSiteId())){
+             cmsPage.setSiteId(queryPageRequest.getSiteId());
+         }
+         //设置模板id
+         if (StringUtils.isNotEmpty(queryPageRequest.getTemplateId())){
+             cmsPage.setTemplateId(queryPageRequest.getTemplateId());
+         }
+         //设置页面别名
+         if (StringUtils.isNotEmpty(queryPageRequest.getPageAliase())){
+             cmsPage.setPageAliase(queryPageRequest.getPageAliase());
+         }
+         //定义条件对象
+         Example<CmsPage> example = Example.of(cmsPage, exampleMatcher);
+         //分页参数
          if (page<=0){
              page=1;
          }
@@ -38,7 +61,8 @@ public class PageService {
          }
          page=page-1;
          Pageable pageable = PageRequest.of(page, size);
-         Page<CmsPage> all = cmsPageRepository.findAll(pageable);
+         //实现自定义条件查询并分页
+         Page<CmsPage> all = cmsPageRepository.findAll(example,pageable);
          QueryResult queryResult=new QueryResult();
          //数据列表
          queryResult.setList(all.getContent());
